8540bd0b54f24e8cef654c3cff7cbd4a0ea8d806
Burke Libbey <burke@libbey.me>
2018-01-26 15:57:40 -0500

    wip

--- lib/todo/commands/help.rb
+++ lib/todo/commands/help.rb
@@ -1,20 +1,15 @@
 require 'todo'
+require 'yaml'
 
 module Todo
   module Commands
     class Help < Todo::Command
       def call(args, _name)
-        puts CLI::UI.fmt("{{bold:Available commands}}")
-        puts ""
-
-        Commands.commands.each do |name, klass_name|
-          next if name == 'help'
-          puts CLI::UI.fmt("{{command:#{CLI::Kit.tool_name} #{name}}}")
-          if help = Todo::Commands.const_get(klass_name).help
-            puts CLI::UI.fmt(help)
-          end
-          puts ""
-        end
+        help_file = File.expand_path('misc/help.yml', Todo::ROOT)
+        help_obj = YAML.load_file(help_file)
+        hb = CLI::Kit::Help.new
+        hb.add_section('Global Commands', help_obj)
+        hb.call(*args)
       end
     end
   end
